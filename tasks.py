f46aae95ad02e5be3c345d434fc55e7a5ba0cf0d
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2016-10-28 18:09:52 +0300

    json

--- tasks.py
+++ tasks.py
@@ -1,25 +1,30 @@
-import argparse
-import pathlib
-import sys
 from pathlib import Path
+import json
 
 class Task(object):
 
-    def __init__(self, task_id, task):
-        self.task_id = task_id
-        self.task = task
+    def __init__(self, id_, text, status='unfinished'):
+        self.id_ = id_
+        self.text = text
+        self.status = status 
 
-    def get_task_id(self):
-        return self.task_id
+    def get_id(self):
+        return self.id_
 
-    def get_task(self):
-        return self.task
+    def get_text(self):
+        return self.text
 
-    def set_id(self, task_id):
-        self.task_id = task_id
+    def get_status(self):
+        return self.status
 
-    def set_task(self, task):
-        self.task = task
+    def set_id(self, id_):
+        self.id_ = id_
+
+    def set_text(self, text):
+        self.text = text
+
+    def set_status(self, status):
+        self.status = status
 
 
 class Tasklist(object):
@@ -30,91 +35,106 @@ class Tasklist(object):
     def __init__(self, name='tasklist', tasksdir='.'):
         self.name = name
         self.tasksdir = tasksdir
-        self.done = {}
-        self.undone = {}
+        self.tasks = {}
         self.limit = 99
 
-        filemap = (self.name, '{}.done'.format(self.name))
-        for filename in filemap:
-            path = Path().resolve().joinpath(self.tasksdir, filename)
-            if path.is_dir():
-                print('Bad path')
-            else:
-                if path.exists():
-                    try:
-                        with path.open() as f:
-                            print(f.readline(), end='')
-                    except IOError:
-                        print('Couldn\'t open {}'.format(str(path)))
+    def get_name(self):
+        return self.name
 
-        # TODO: read tasks from tasklist file
+    def get_tasks(self):
+        return self.tasks
 
-    def add_task(self, task_text):
+    def set_name(self, name):
+        self.name = name
+
+    def add(self, text):
         if self.num_of_tasks <= self.limit and len(self.free_ids) != 0:
-            task_id = self.free_ids.pop(0)
-            task = Task(task_id, task_text)
-            self.undone[task.task_id] = task
+            id_ = self.free_ids.pop(0)
+            task = Task(id_, text)
+            self.tasks[task.id_] = task
             self.num_of_tasks += 1
 
-    def remove_task(self, task_id):
-        if task_id in self.done:
-            self.done.pop(task_id)
+    def remove(self, id_):
+        if id_ in self.tasks:
+            self.tasks.pop(id_)
             self.num_of_tasks -= 1
-            self.free_ids.append(task_id)
-        if task_id in self.undone:
-            self.undone.pop(task_id)
-            self.num_of_tasks -= 1
-            self.free_ids.append(task_id)
+            self.free_ids.append(id_)
 
-    def change_task(self, task_id, task):
-        if task_id in self.done:
-            self.done[task_id].set_task(task)
-        if task_id in self.undone:
-            self.undone[task_id].set_task(task)
+    def change(self, id_, text):
+        if id_ in self.tasks:
+            self.tasks[id_].set_task(text)
 
-    def do_task(self, task_id):
-        if task_id in self.undone:
-            self.done[task_id] = self.undone.pop(task_id)
+    def finish(self, id_):
+        if id_ in self.tasks:
+            self.tasks[id_].set_status('finished')
 
-    def undo_task(self, task_id):
-        if task_id in self.done:
-            self.undone[task_id] = self.done.pop(task_id)
-
-    def do_all_tasks(self):
-        temp_dict = self.undone.copy()
+    def finish_all(self):
+        temp_dict = self.tasks.copy()
         for k in temp_dict:
-            self.done[k] = self.undone.pop(k)
+            self.tasks[k].set_status('finished')
 
-    def undo_all_tasks(self):
-        temp_dict = self.done.copy()
-        for k in temp_dict:
-            self.undone[k] = self.done.pop(k)
-
-    def clear_all_tasks(self):
-        self.done.clear()
-        self.undone.clear()
+    def clear_all(self):
+        self.tasks.clear()
         self.free_ids = [i for i in range(1, 100)]
         self.num_of_tasks = 0
 
-    def list_all_tasks(self):
-        print('\n' + self.name.upper())
-        print('=' * len(self.name))
+    def list_all(self):
+        print('\n' + self.get_name().upper())
+        print('=' * len(self.get_name()))
         if self.num_of_tasks == 0:
             print('No tasks')
         else:
-            for k in self.done.keys():
-                print('{:2}. [X] {}'.format(self.done[k].task_id,
-                                            self.done[k].task))
-            for k in self.undone.keys():
-                print('{:2}. [ ] {}'.format(self.undone[k].task_id,
-                                            self.undone[k].task))
-        print('=' * len(self.name))
-        print('{}/{}\n'.format( self.num_of_tasks, self.limit))
+            for k in self.tasks.keys():
+                print('{:2}. [{}] {}'.format(self.tasks[k].get_id(),
+                    'X' if self.tasks[k].get_status() == 'finished' else ' ',
+                    self.tasks[k].get_text()))
+        print('=' * len(self.get_name()), '\n')
+
+
+def write_tasks(tasks, f):
+    ts = tasks.get_tasks()
+    for k in ts:
+        id_ = ts[k].get_id()
+        text = ts[k].get_text()
+        status = ts[k].get_status()
+        data = json.dumps({id_: (text, status)}, ensure_ascii=False,
+                                                 sort_keys=True)
+        f.write(data + '\n')
+
+
+def read_tasks(tasks, f):
+    pass
 
 
 def main():
     tasks = Tasklist()
-    tasks.list_all_tasks()
+    tasks.add('Math')
+    tasks.add('Programming')
+    tasks.add('Jogging')
+    tasks.finish(1)
+    tasks.finish(3)
+    tasks.list_all()
+
+    tasksdir = '.'
+    path = Path().resolve().joinpath(tasksdir, tasks.get_name() + '.json')
+
+    if path.is_dir():
+        # TODO: raise custom exception
+        pass
+    else:
+        if path.exists():
+            try:
+                with path.open('w') as f:
+                    write_tasks(tasks, f)
+            except IOError:
+                # TODO: raise custom exception
+                pass
+
+    with path.open('r') as f:
+        lines = f.readlines()
+        for line in lines:
+            data = json.loads(line)
+            print(data)
 
 
 if __name__ == '__main__':
