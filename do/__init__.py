cfc7efe7e315603e71052c36b021a0ed661638d2
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2016-11-06 14:35:20 +0200

    update

---
+++ do/__init__.py
@@ -0,0 +1,4 @@
+import do
+
+__author__ = 'Vitaliy Pisnya <vitaliy.pisnya@gmail.com>'
+__version__ = (1, 0, 0)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='do',
+      version='0.1',
+      description='Simple command-line todo app',
+      url='https://github.com/weezybusy/do.git',
+      author='Vitaliy Pisnya',
+      author_email='viatliy.pisnya@gmail.com',
+      license='MIT',
+      packages=['do'],
+      zip_safe=False)
